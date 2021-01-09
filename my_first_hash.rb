ee25b6b02cdb1a5c12367e58519b99a104158e4a
Sara Nolen <nolensara1@gmail.com>
2021-01-09 00:21:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-your_hash = {name => "Sara", age => "36", activities => "biking" "running" "hiking", pets => "Luna" "Sol"}
+your_hash = {"name" => "Sara", "age" => "36", "activities" => "biking" "running" "hiking", "pets" => "Luna" "Sol"}
 
 end
 
